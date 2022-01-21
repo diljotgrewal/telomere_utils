fdd520a50f2c3cce2bb9699d42c00b32d3734608
Diljot Grewal <diljot.grewal@gmail.com>
2022-01-21 11:06:47 -0800

    expanded search space for telomeres

--- telomere_utils/merge.py
+++ telomere_utils/merge.py
@@ -5,14 +5,10 @@ def merge(infiles, outfile):
     infiles = [pd.read_csv(filepath) for filepath in infiles]
     infiles = pd.concat(infiles)
 
-    infiles['start_1'] = infiles['start_1'].astype("Int64")
-    infiles['end_1'] = infiles['end_1'].astype("Int64")
-    infiles['telomere_start_1'] = infiles['telomere_start_1'].astype("Int64")
-    infiles['telomere_end_1'] = infiles['telomere_end_1'].astype("Int64")
+    infiles['start'] = infiles['start'].astype("Int64")
+    infiles['end'] = infiles['end'].astype("Int64")
+    infiles['telomere_start'] = infiles['telomere_start'].astype("Int64")
+    infiles['telomere_end'] = infiles['telomere_end'].astype("Int64")
 
-    infiles['start_2'] = infiles['start_2'].astype("Int64")
-    infiles['end_2'] = infiles['end_2'].astype("Int64")
-    infiles['telomere_start_2'] = infiles['telomere_start_2'].astype("Int64")
-    infiles['telomere_end_2'] = infiles['telomere_end_2'].astype("Int64")
 
     infiles.to_csv(outfile, index=False, na_rep='NA')
--- telomere_utils/overlap.py
+++ telomere_utils/overlap.py
@@ -36,26 +36,14 @@ def build_germline_ref(bamfile, telomeres, binsize=1000):
 
     for i, row in telomeres.iterrows():
 
-        if not pd.isna(row['telomere_start_1']):
-            chromosome_1 = row['chromosome_1']
-            telomere_start_1 = int(row['start_1'] + row['telomere_start_1'])
-            telomere_end_1 = int(row['start_1'] + row['telomere_end_1'])
-            binval_start = get_overlapping_bin(bamfile, chromosome_1, telomere_start_1, binsize=binsize)
-            binval_end = get_overlapping_bin(bamfile, chromosome_1, telomere_end_1, binsize=binsize)
+        chromosome = row['chromosome']
+        telomere_start = int(row['start'] + row['telomere_start'])
+        telomere_end = int(row['start'] + row['telomere_end'])
+        binval_start = get_overlapping_bin(bamfile, chromosome, telomere_start, binsize=binsize)
+        binval_end = get_overlapping_bin(bamfile, chromosome, telomere_end, binsize=binsize)
 
-            outdata[binval_start] = 1
-            outdata[binval_end] = 1
-        else:
-            assert not pd.isna(row['telomere_start_2'])
-            chromosome_2 = row['chromosome_2']
-            telomere_start_2 = int(row['start_2'] + row['telomere_start_2'])
-            telomere_end_2 = int(row['start_2'] + row['telomere_end_2'])
-
-            binval_start = get_overlapping_bin(bamfile, chromosome_2, telomere_start_2, binsize=binsize)
-            binval_end = get_overlapping_bin(bamfile, chromosome_2, telomere_end_2, binsize=binsize)
-
-            outdata[binval_start] = 1
-            outdata[binval_end] = 1
+        outdata[binval_start] = 1
+        outdata[binval_end] = 1
 
     return outdata
 
@@ -76,29 +64,16 @@ def build_counts(bamfile, telomeres, output, binsize=1000):
 
     for i, row in telomeres.iterrows():
 
-        if not pd.isna(row['telomere_start_1']):
-            chromosome_1 = row['chromosome_1']
-            telomere_start_1 = int(row['start_1'] + row['telomere_start_1'])
-            telomere_end_1 = int(row['start_1'] + row['telomere_end_1'])
-            binval_start = get_overlapping_bin(bamfile, chromosome_1, telomere_start_1, binsize=binsize)
-            binval_end = get_overlapping_bin(bamfile, chromosome_1, telomere_end_1, binsize=binsize)
-            if binval_start == binval_end:
-                outdata[binval_start] += 1
-            else:
-                outdata[binval_start] += 1
-                outdata[binval_end] += 1
+        chromosome = row['chromosome']
+        telomere_start = int(row['start'] + row['telomere_start'])
+        telomere_end = int(row['start'] + row['telomere_end'])
+        binval_start = get_overlapping_bin(bamfile, chromosome, telomere_start, binsize=binsize)
+        binval_end = get_overlapping_bin(bamfile, chromosome, telomere_end, binsize=binsize)
+        if binval_start == binval_end:
+            outdata[binval_start] += 1
         else:
-            assert not pd.isna(row['telomere_start_2'])
-            chromosome_2 = row['chromosome_2']
-            telomere_start_2 = int(row['start_2'] + row['telomere_start_2'])
-            telomere_end_2 = int(row['start_2'] + row['telomere_end_2'])
-            binval_start = get_overlapping_bin(bamfile, chromosome_2, telomere_start_2, binsize=binsize)
-            binval_end = get_overlapping_bin(bamfile, chromosome_2, telomere_end_2, binsize=binsize)
-            if binval_start == binval_end:
-                outdata[binval_start] += 1
-            else:
-                outdata[binval_start] += 1
-                outdata[binval_end] += 1
+            outdata[binval_start] += 1
+            outdata[binval_end] += 1
 
     sorted_counts = sorted(outdata.items(), key=itemgetter(1))
     outdata = []
@@ -128,27 +103,15 @@ def filter_telomeres(tumour_telomeres, normal_data, output, bamfile, binsize=100
     for i, row in tumour_telomeres.iterrows():
         row['germline'] = False
 
-        if not pd.isna(row['telomere_start_1']):
-            chromosome_1 = row['chromosome_1']
-            telomere_start_1 = int(row['start_1'] + row['telomere_start_1'])
-            telomere_end_1 = int(row['start_1'] + row['telomere_end_1'])
+        chromosome = row['chromosome']
+        telomere_start = int(row['start'] + row['telomere_start'])
+        telomere_end = int(row['start'] + row['telomere_end'])
 
-            binval_start = get_overlapping_bin(bamfile, chromosome_1, telomere_start_1, binsize=binsize)
-            binval_end = get_overlapping_bin(bamfile, chromosome_1, telomere_end_1, binsize=binsize)
+        binval_start = get_overlapping_bin(bamfile, chromosome, telomere_start, binsize=binsize)
+        binval_end = get_overlapping_bin(bamfile, chromosome, telomere_end, binsize=binsize)
 
-            if normal_data[binval_start] or normal_data[binval_end]:
-                row['germline'] = True
-
-        else:
-            assert not pd.isna(row['telomere_start_2'])
-            chromosome_2 = row['chromosome_2']
-            telomere_start_2 = int(row['start_2'] + row['telomere_start_2'])
-            telomere_end_2 = int(row['start_2'] + row['telomere_end_2'])
-
-            binval_start = get_overlapping_bin(bamfile, chromosome_2, telomere_start_2, binsize=binsize)
-            binval_end = get_overlapping_bin(bamfile, chromosome_2, telomere_end_2, binsize=binsize)
-            if normal_data[binval_start] or normal_data[binval_end]:
-                row['germline'] = True
+        if normal_data[binval_start] or normal_data[binval_end]:
+            row['germline'] = True
 
         alldata.append(row)
 
